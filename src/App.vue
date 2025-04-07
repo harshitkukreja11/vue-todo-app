836db4a3f726d1c6fd251bd422424b261dc87c53
harshitkukreja11 <harshit.kukreja11@gmail.com>
2025-04-07 12:31:09 +0530

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,7 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <TodoApp />
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import TodoApp from './components/TodoApp.vue';
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/TodoApp.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <h1>Vue CLI Todo App</h1>
+    <input v-model="newTodo" @keyup.enter="addTodo" placeholder="Add a task" />
+    <button @click="addTodo">Add</button>
+
+    <ul>
+      <li v-for="(todo, index) in todos" :key="index">
+        {{ todo }}
+        <button @click="removeTodo(index)">Remove</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const newTodo = ref('');
+const todos = ref([]);
+
+const addTodo = () => {
+  if (newTodo.value.trim()) {
+    todos.value.push(newTodo.value.trim());
+    newTodo.value = '';
+  }
+};
+
+const removeTodo = (index) => {
+  todos.value.splice(index, 1);
+};
+</script>
+
+<style scoped>
+input {
+  margin-right: 10px;
+}
+li {
+  margin: 8px 0;
+}
+</style>
